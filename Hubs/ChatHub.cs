a1a9c2fc4da2438f6460e313f36101e4e1b52de1
EddyBautista <bautista.j.eddy@gmail.com>
2022-01-23 11:09:33 -0600

    Fix a few errors in got the function working

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,15 +1,12 @@
-using System.Net.Http;
 using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChat.Hubs
 {
-    // Hubs handle Client-Server Communication - a SingalR base class
-    public class ChatHub : Hubs
+    public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
-        
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingalR();
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
